e6136106173be80779fe96f506e2a16a95bb3663
naveenkaratekid <naveen.hopetech@gmail.com>
2018-01-26 14:30:21 -0800

    Created Cone object
    Created cone object

--- Assignment3/cone.js
+++ Assignment3/cone.js
@@ -2,8 +2,9 @@ var gl = null;
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
-
+    
     gl = WebGLUtils.setupWebGL( canvas );
+    var cone = new Cone(gl, 8, "Cone-vertex-shader", "Cone-fragment-shader");
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
