65955b89171359d5d0924e5576fd395bdd70a153
naveenkaratekid <naveen.hopetech@gmail.com>
2018-01-26 16:13:03 -0800

    Update cone.js

--- Assignment3/cone.js
+++ Assignment3/cone.js
@@ -1,16 +1,16 @@
 var gl = null;
-
+var cone = null;
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
-    var cone = new Cone(gl, 4, "Cone-vertex-shader", "Cone-fragment-shader");
+    
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
     }
-
+    cone = new Cone(gl, 100, "Cone-vertex-shader", "Cone-fragment-shader");
     gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
 
     render();
