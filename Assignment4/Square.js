7335d57e07206963a76ef81675299dfd48d5447c
naveenkaratekid <naveen.hopetech@gmail.com>
2018-02-01 16:05:04 -0800

    Update Square.js

--- Assignment4/Square.js
+++ Assignment4/Square.js
@@ -36,7 +36,8 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 		numComponents : 3 
 	};
     this.indices = {
-    values : new Uint16Array([ 0, 1, 3, 2 ])
+    values : new Uint16Array([ 1, 3, 2 ])
+    //values : new Uint16Array([ 0, 1, 3, 2 ])
     };
 	
 	// positions
@@ -77,7 +78,8 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 		var start = 0;
 		var count = this.count;
 		//gl.drawArrays(gl.TRIANGLE_STRIP, start, count); // TRIANGLE_STRIP
-		gl.drawElements(gl.TRIANGLE_STRIP, this.indices.values.length, gl.UNSIGNED_SHORT, 0);
+		//gl.drawElements(gl.TRIANGLE_STRIP, this.indices.values.length, gl.UNSIGNED_SHORT, 0);
+		gl.drawElements(gl.TRIANGLES, this.indices.values.length, gl.UNSIGNED_SHORT, 0);
 
 
 	};
