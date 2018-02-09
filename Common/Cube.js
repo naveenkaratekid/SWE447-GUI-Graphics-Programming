8a298180148e98dd2f383b9946d06d78f0928d34
naveenkaratekid <naveen.hopetech@gmail.com>
2018-02-08 20:35:41 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -18,110 +18,108 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.positions = { 
         values : new Float32Array([
            // Front face
-	  -0.5, -0.5,  0.5,
-	   0.5, -0.5,  0.5,
-	   0.5,  0.5,  0.5,
-	  -0.5,  0.5,  0.5,
-
-	  // Rear face
-	  -0.5, -0.5, -0.5,
-	  -0.5,  0.5, -0.5,
-	   0.5,  0.5, -0.5,
-	   0.5, -0.5, -0.5,
-
-	  // Top face
-	  -0.5,  0.5, -0.5,
-	  -0.5,  0.5,  0.5,
-	   0.5,  0.5,  0.5,
-	   0.5,  0.5, -0.5,
-
-	  // Bottom face
-	  -0.5, -0.5, -0.5,
-	   0.5, -0.5, -0.5,
-	   0.5, -0.5,  0.5,
-	  -0.5, -0.5,  0.5,
-
-	  // Right face
-	   0.5, -0.5, -0.5,
-	   0.5,  0.5, -0.5,
-	   0.5,  0.5,  0.5,
-	   0.5, -0.5,  0.5,
-
-	  // Left face
-	  -0.5, -0.5, -0.5,
-	  -0.5, -0.5,  0.5,
-	  -0.5,  0.5,  0.5,
-	  -0.5,  0.5, -0.5
+  -0.5, -0.5,  0.5,
+   0.5, -0.5,  0.5,
+   0.5,  0.5,  0.5,
+  -0.5,  0.5,  0.5,
+  
+  // Back face
+  -0.5, -0.5, -0.5,
+  -0.5,  0.5, -0.5,
+   0.5,  0.5, -0.5,
+   0.5, -0.5, -0.5,
+  
+  // Top face
+  -0.5,  0.5, -0.5,
+  -0.5,  0.5,  0.5,
+   0.5,  0.5,  0.5,
+   0.5,  0.5, -0.5,
+  
+  // Bottom face
+  -0.5, -0.5, -0.5,
+   0.5, -0.5, -0.5,
+   0.5, -0.5,  0.5,
+  -0.5, -0.5,  0.5,
+  
+  // Right face
+   0.5, -0.5, -0.5,
+   0.5,  0.5, -0.5,
+   0.5,  0.5,  0.5,
+   0.5, -0.5,  0.5,
+  
+  // Left face
+  -0.5, -0.5, -0.5,
+  -0.5, -0.5,  0.5,
+  -0.5,  0.5,  0.5,
+  -0.5,  0.5, -0.5
             ]),
         numComponents : 3
     };
-    this.colors = 
-    {
-	values : new Float32Array([
-		// Front
-		0.0, 0.6, 1.0,
-		0.0, 0.6, 1.0,
-		0.0, 0.6, 1.0,
-		0.0, 0.6, 1.0,
-
-		// Rear face
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-
-		// Top face
-		1.0, 0.2, 0.0,
-		1.0, 0.2, 0.0,
-		1.0, 0.2, 0.0,
-		1.0, 0.2, 0.0,
-
-		// Bottom
-		1.0, 0.6, 0.0,
-		1.0, 0.6, 0.0,
-		1.0, 0.6, 0.0,
-		1.0, 0.6, 0.0,
-
-		// Right
-		0.0, 0.2, 0.4,
-		0.0, 0.2, 0.4,
-		0.0, 0.2, 0.4,
-		0.0, 0.2, 0.4,
-
-		// Left
-		0.4, 0.0, 0.4,
-		0.4, 0.0, 0.4,
-		0.4, 0.0, 0.4,
-		0.4, 0.0, 0.4
-	]),
-	    numComponents : 3
-    };
+    
+    this.colors = {
+		values : new Float32Array([
+		     // Front face
+   0.5,  0.5,  0.0,
+   0.5,  0.5,  0.0,
+   0.5,  0.5,  0.0,
+   0.5,  0.5,  0.0,
+  
+  // Back face
+  0.75, 0.5, 0.5,
+  0.75, 0.5, 0.5,
+  0.75, 0.5, 0.5,
+  0.75, 0.5, 0.5,
+  
+  // Top face
+  0.5,  0.0, 0.5,
+  0.5,  0.0, 0.5,
+  0.5,  0.0, 0.5,
+  0.5,  0.0, 0.5,
+  
+  // Bottom face
+  0.0, 0.25, 0.75,
+  0.0, 0.25, 0.75,
+  0.0, 0.25,  0.75,
+  0.0, 0.25,  0.75,
+  
+  // Right face
+   0.0,  0.0,  0.5,
+   0.0,  0.0,  0.5,
+   0.0,  0.0,  0.5,
+   0.0,  0.0,  0.5,
+  
+  // Left face
+   0.5,  0.0, 0.0,
+   0.5,  0.0, 0.0,
+   0.5,  0.0, 0.0,
+   0.5,  0.0, 0.0
+		]),
+		numComponents : 3 
+	};
     
     this.indices = { 
         values : new Uint16Array([
-        0, 2,  1,  0,  3,  2,    // front face
-    	4, 6,  5, 4,  7,  6,    // back face
-    	8, 10, 9, 8,  11, 10,   // top face
-    	12, 14, 13, 12, 15, 14,   // bottom face
-    	16, 18, 17, 16, 19, 18,   // right face
-    	20, 22, 21, 20, 23, 22   // left face
+        0,  2,  1,      0,  3,  2,    // front
+    	4,  6,  5,      4,  7,  6,    // back
+    	8,  10,  9,     8,  11, 10,   // top
+    	12, 14, 13,     12, 15, 14,   // bottom
+    	16, 18, 17,     16, 19, 18,   // right
+    	20, 22, 21,     20, 23, 22   // left
         ])
     };
     this.indices.count = this.indices.values.length;
 
-    // psotion buffer
+    
     this.positions.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
     gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
 
-	// color buffer
     this.colors.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
     gl.bufferData( gl.ARRAY_BUFFER, this.colors.values, gl.STATIC_DRAW );
     this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor" );
-    gl.enableVertexAttribArray( this.colors.attributeLoc );
+    gl.enableVertexAttribArray( this.colors.attributeLoc );	
 	
-	// indices buffer
     this.indices.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
     gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, this.indices.values, gl.STATIC_DRAW );
@@ -139,12 +137,12 @@ function Cube( vertexShaderId, fragmentShaderId ) {
         gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
         gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
             gl.FLOAT, gl.FALSE, 0, 0 );
- 
-        gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+	    
+	gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
     	gl.vertexAttribPointer( this.colors.attributeLoc, this.colors.numComponents,
         		gl.FLOAT, gl.FALSE, 0, 0 );
-	    
-	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+ 
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
 
         gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
 
