cd2d787053cae4ffab65d68a4f8d8f611fe17c15
naveenkaratekid <naveen.hopetech@gmail.com>
2018-02-25 09:45:30 -0800

    Line 213 and Line 214

--- Assignment 6/rCube.js
+++ Assignment 6/rCube.js
@@ -210,8 +210,8 @@ function Cube(gl, vertexShaderId, fragmentShaderId) {
 
     // Texture
     this.initTexture();
-    //this.texture = loadTexture(gl, "/home/saeid/Projects/WebGL/webgl-examples/tutorial/Mysample6/cubetexture.png");
-    //texture = loadTexture(gl, "cubetexture.png");
+    this.texture = loadTexture(gl, "cubetexture.png");
+    texture = loadTexture(gl, "cubetexture.png");
     
     this.textures.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.textures.buffer);
