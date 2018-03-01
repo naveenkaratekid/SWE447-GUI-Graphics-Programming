ea24e634840dfad886ccf4ed99e4d85a32b353f8
naveenkaratekid <naveen.hopetech@gmail.com>
2018-03-01 15:37:02 -0800

    Add files via upload

---
+++ Midterm Problem 1/cone.js
@@ -0,0 +1,26 @@
+var gl = null;
+var cone = null;
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    cone = new Cone(gl, 100);
+    
+    //gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
+    gl.clearColor( 0.0, 0.0, 1.0, 1.0 );
+    render();
+}
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
+}
+
+window.onload = init;
