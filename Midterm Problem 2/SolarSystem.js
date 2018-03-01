cc4201b910d850c2abe8fcb9ef6678e19d33811e
naveenkaratekid <naveen.hopetech@gmail.com>
2018-03-01 15:46:25 -0800

    Add files via upload

---
+++ Midterm Problem 2/SolarSystem.js
@@ -0,0 +1,68 @@
+var SolarSystem = {
+  Sun : {
+    radius : 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.76,
+    distance : 0.687,
+    year : 0.241,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 0.95,
+    distance : 0.73,
+    year : 0.615,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 1,
+    distance : 1,
+    year : 1,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.273,
+    distance : 0.0026,
+    year : 0.0748,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.534,
+    distance : 1.54,
+    year : 1.88,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 4.00,
+    distance : 2.8,
+    year : 11.9,
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 3.19,
+    distance : 3.7,
+    year : 29.4,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 2.69,
+    distance : 4.8,
+    year : 83.7,
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 2.29,
+    distance : 5.9,
+    year : 163.7,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.585,
+    distance : 6.7,
+    year : 247.9,
+    color : [ 150/255, 75/255, 0.0, 1.0 ]
+  }
+};
